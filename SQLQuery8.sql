c186ceb419c330c114d0285c03b3212b876d4092
Isabele Pinto Ferreira Silva de Oliveira <isabelepfso@gmail.com>
2023-02-14 11:02:06 -0300

    activity 8

---
+++ SQLQuery8.sql
@@ -0,0 +1,4 @@
+/*
+A empresa decidiu que todas as televisões de LCD receberão um super desconto no próximo
+mês. O seu trabalho é fazer uma consulta à tabela DimProduct e retornar os ID’s, Nomes e
+Preços de todos os produtos TV LCD existentes.*/SELECT	ProductKey,	ProductName,	UnitPriceFROM 	DimProductWHERE ProductName LIKE '%LCD%' AND ProductName LIKE '%TV%'
