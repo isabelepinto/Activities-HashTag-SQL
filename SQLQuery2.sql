8da70f154319a88859f670aaf352399f66a82d37
Isabele Pinto Ferreira Silva de Oliveira <isabelepfso@gmail.com>
2023-02-14 09:38:18 -0300

    first commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+/*
+Os TOP 10 produtos com maior UnitPrice possuem exatamente o mesmo preço. 
+Porém, a empresa quer diferenciar esses preços de acordo com o peso (Weight) de cada um.
+O que você precisará fazer é ordenar esses top 10 produtos, de acordo com a coluna de
+UnitPrice e, além disso, estabelecer um critério de desempate, para que seja mostrado na
+ordem, do maior para o menor.
+Caso ainda assim haja um empate entre 2 ou mais produtos, pense em uma forma de criar
+um segundo critério de desempate (além do peso).*/SELECT 	TOP(10) *FROM 	DimProductORDER BY UnitPrice DESC, Weight DESC, Size DESC
---
+++ SQLQuery4.sql
@@ -0,0 +1,7 @@
+/*
+Você foi alocado para criar um relatório das lojas registradas atualmente na Contoso.
+a) Descubra quantas lojas a empresa tem no total. Na consulta que você deverá fazer à tabela
+DimStore, retorne as seguintes informações: StoreName, OpenDate, EmployeeCount
+b) Renomeeie as colunas anteriores para deixar a sua consulta mais intuitiva.
+c) Dessas lojas, descubra quantas (e quais) lojas ainda estão ativas.
+*/
